355e48126a98daeef3fba55efa27fd797b9c0c8e
Johnny Olsen <johnnyolsen90@gmail.com>
2022-01-14 14:18:03 +0100

    Added board and show board test

--- src/main/java/GoLSimulation.java
+++ src/main/java/GoLSimulation.java
@@ -1,2 +1,11 @@
 public class GoLSimulation {
+    int width;
+    int height;
+    int[][] gameOfLifeBoard;
+
+    public GoLSimulation(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.gameOfLifeBoard = new int[width][height];
+    }
 }
--- src/test/java/GoLSimulationTest.java
+++ src/test/java/GoLSimulationTest.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoLSimulationTest {
-
+    GoLSimulation simulation;
     @BeforeEach
     void setUp() {
+        simulation = new GoLSimulation(10, 10);
+    }
+
+    @Test
+    void tenByTenBoardExists() {
+        int checkZeroOnGivenPosition = 0;
+        int givenPosition = simulation.gameOfLifeBoard[9][9];
+        assertEquals(checkZeroOnGivenPosition, givenPosition);
     }
 }
